2141e83e8a99b3947111e4b48c654e43ff8c427e
Anar Hummatli <hummatli57@gmail.com>
2025-07-12 05:36:06 +0400

    feat: add UserController with registration endpoint

---
+++ src/main/java/com/fitness/fitnesstrackerapi/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.fitness.fitnesstrackerapi.controller;
+
+import com.fitness.fitnesstrackerapi.model.User;
+import com.fitness.fitnesstrackerapi.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User createdUser = userService.registerUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+}
