9abe2d59380a5424a5767c897ac4a87f4ec41c82
Anar Hummatli <hummatli57@gmail.com>
2025-07-12 04:47:12 +0400

    feat: add UserRepository with findByEmail and existsByEmail methods

--- src/main/java/com/fitness/fitnesstrackerapi/model/User.java
+++ src/main/java/com/fitness/fitnesstrackerapi/model/User.java
@@ -9,7 +9,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class user {
+@Table(name = "\"user\"")
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
